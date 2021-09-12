97d3b51f4fec2bd8c707f3ebc22160c46cfeff93
IyedK <iyed8000@gmail.com>
2021-09-12 13:18:25 +0200

    Update securityConfiguration.java

--- hapt/src/main/java/com/horizon/hapt/configurations/securityConfiguration.java
+++ hapt/src/main/java/com/horizon/hapt/configurations/securityConfiguration.java
@@ -1,5 +1,48 @@
 package com.horizon.hapt.configurations;
 
-public class securityConfiguration {
+import javax.activation.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+/**
+ * classe du configuration
+ */
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "/js/**", "/css/**", "/scss/**", "/vendor/**", "/img/**", "/fonts/**", "/api/**",
+                        "/init/**")
+                .permitAll().antMatchers("/login", "/index").permitAll().antMatchers("/admin/**").hasRole("ADMIN")/*
+                                                                                                                   * l'
+                                                                                                                   * admin
+                                                                                                                   * w
+                                                                                                                   * ili
+                                                                                                                   * ta7tou
+                                                                                                                   **/
+                .anyRequest().authenticated()
+                // Login form
+                .and().formLogin().loginPage("/login").usernameParameter("tel").passwordParameter("password")
+                // Fail, forbidden and Success actions
+                .failureUrl("/login?error").defaultSuccessUrl("/account").and().exceptionHandling()
+                .accessDeniedPage("/forbidden")
+                // Logout listener setting and action
+                .and().logout().invalidateHttpSession(true).clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login?logout")
+                .permitAll()
+                // CSRF disabling
+                .and().csrf().disable();
+    }
 
 }
